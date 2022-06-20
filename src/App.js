b99268090acb3a25227aeac234f588c8c9d2b235
AngelGO23 <ortizangelg23@gmail.com>
2022-06-20 15:25:09 -0500

    first dispatch to reducer created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.scss';
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+
+
 
 function App() {
   return (
-    <div className="App">
-     App
+    <div className="app">
+     <Router>
+     <Header></Header>
+     <div className='container'>
+        <Routes>
+            
+            <Route path="/"  exact element= {<Home/>} />
+            <Route path="/movie/:imdbID" exact element={<MovieDetails/>}/>
+            <Route element={PageNotFound}/>
+            
+        </Routes>
+      </div>
+      <Footer />
+     </Router>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MovieListing from "../MovieListing/MovieListing";
+import movieAPI from "../../common/apis/movieAPI";
+import {APIKey} from "../../common/apis/movieAPIKey"
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
+
 
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
+
+  
+  const fetchMovies = async () =>{
+    const response = await movieAPI.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+    .catch((err) =>{
+      console.log("err", err);
+    });
+    dispatch(addMovies(response.data));
+  }
+  
+  useEffect(()=>{
+
+    fetchMovies();
+
+  },[])
   return (
-    <div>Home</div>
+    <div>
+    <div className='banner-img'></div>
+    <MovieListing/>
+    </div>
   )
 }
 
-export default Home
+export default Home;
